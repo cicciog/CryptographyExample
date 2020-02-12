19c25c0398d3fa6a483c95183de6fdbabc0da08b
cicciog <cicciog>
2020-02-12 11:41:23 +0100

    The MAC algorithm  was updated and the verification algorithm has been implemented

--- src/main/java/MessageAuthenticationCode/MacAlgorithm.java
+++ src/main/java/MessageAuthenticationCode/MacAlgorithm.java
@@ -1,4 +1,29 @@
 package MessageAuthenticationCode;
 
+import javax.crypto.Mac;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
 public class MacAlgorithm {
+    private String message;
+    private Key key;
+    private Mac mac;
+
+    public MacAlgorithm(Key pKey,String pMessage) throws NoSuchAlgorithmException {
+        //obtain key and a message
+        this.key = pKey;
+        this.message = pMessage;
+        //Creating a Mac object
+        this.mac = Mac.getInstance("HmacSHA256");
+    }
+
+    public byte[] MacOperation() throws InvalidKeyException {
+        //Initializing the Mac object
+        mac.init(this.key);
+        //Computing the Mac
+        byte[] msg = this.message.getBytes();
+        byte[] macResult = mac.doFinal(msg);
+        return macResult;
+    }
 }
--- src/main/java/MessageAuthenticationCode/VerifyAlgorithm.java
+++ src/main/java/MessageAuthenticationCode/VerifyAlgorithm.java
@@ -1,5 +1,31 @@
 package MessageAuthenticationCode;
 
-//not implemented yet
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 public class VerifyAlgorithm {
+    private String message;
+    private Key key;
+    private byte[] tag;
+
+    public VerifyAlgorithm(String pMessage, Key pKey, byte[] pTag) {
+        this.message = pMessage;
+        this.key = pKey;
+        this.tag = pTag;
+    }
+
+    public String checkTagValidity(byte[] pTag) throws NoSuchAlgorithmException, InvalidKeyException {
+
+        MacAlgorithm macAlgorithm = new MacAlgorithm(this.key, this.message);
+        byte[] computedTag = macAlgorithm.MacOperation();
+
+        if (Arrays.equals(pTag,computedTag)){
+            return "authentic";
+        }else{
+            return "not authentic";
+        }
+
+    }
 }
